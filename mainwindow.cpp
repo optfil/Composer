dee9e6efeafe028a4ffcea839135a320a90587d1
Alex <dergachev88@gmail.com>
2019-12-10 16:23:48 +0300

    Create intermediate widget for central region

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,15 +4,18 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QWidget * widgetCentral = new QWidget;
+    setCentralWidget(widgetCentral);
+
     problem.setTask("Условие");
     problem.setSolution("Решение");
 
     problemWidget = new ProblemWidget;
 
-//    QHBoxLayout * layoutMain = new QHBoxLayout;
-//    layoutMain->addWidget(problemWidget);
+    QHBoxLayout * layoutMain = new QHBoxLayout;
+    layoutMain->addWidget(problemWidget);
 
-    setCentralWidget(problemWidget);
+    widgetCentral->setLayout(layoutMain);
 
     problemWidget->updateProblem(problem);
 }
