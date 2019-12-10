6283d2ab4d895797d24c4a0923162ff40cb8c766
Alex <dergachev88@gmail.com>
2019-12-10 17:37:55 +0300

    Main menu created

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,16 @@
 #include "mainwindow.h"
 #include <QLayout>
 #include <QStatusBar>
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QWidget * widgetCentral = new QWidget;
-    setCentralWidget(widgetCentral);
-
     problem.setTask("Условие");
     problem.setSolution("Решение");
+
+    QWidget * widgetCentral = new QWidget;
+    setCentralWidget(widgetCentral);
 
     problemWidget = new ProblemWidget;
 
@@ -17,11 +18,18 @@ MainWindow::MainWindow(QWidget *parent)
     layoutMain->addWidget(problemWidget);
 
     widgetCentral->setLayout(layoutMain);
-
+    statusBar();  // needed to create status bar
 //    statusBar()->showMessage("message here");
 //    setWindowTitle("Composer 0.1");
 //    setMinimumSize(300, 300);
 //    resize(480, 320);
+
+    QAction * actionQuit = new QAction(tr("&Quit"), this);
+//    actionQuit->setShortcuts(QKeySequence::New);
+    actionQuit->setStatusTip(tr("Close application"));
+    connect(actionQuit, &QAction::triggered, this, &MainWindow::close);
+    QMenu * menuFile = menuBar()->addMenu(tr("&File"));
+    menuFile->addAction(actionQuit);
 
     problemWidget->updateProblem(problem);
 }
