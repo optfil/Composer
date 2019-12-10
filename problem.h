2c39e6376911c00548fbac327df43af65f37e75c
Alex <dergachev88@gmail.com>
2019-12-10 16:14:52 +0300

    Add getters

--- problem.h
+++ problem.h
@@ -10,6 +10,8 @@ public:
 
     void setTask(const QString & task) {task_ = task;}
     void setSolution(const QString & solution) {solution_ = solution;}
+    QString task() const {return task_;}
+    QString solution() const {return solution_;}
 
 private:
     QString task_;
