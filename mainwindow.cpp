5dfde7e0d4fc32929d5fa03386cff293dfbfcb2e
Alexander Dergachev <dergachev88@gmail.com>
2020-07-19 12:16:27 +0300

    comment removed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -103,24 +103,3 @@ void MainWindow::processError(const QSqlError& error)
     textEditResponse->clear();
     textEditResponse->append(error.text());
 }
-
-/*
-void MainWindow::mock_create_problems()
-{
-    problems.append(Problem("Задача про колесо"));
-    problems.last().setTask("Колесо катится без проскальзывания. Найти его скорость.");
-    problems.last().setSolution("Скорость равна 2 м/с.");
-
-    problems.append(Problem("Задача про уробороса"));
-    problems.last().setTask("Уроборос плотно пообедал. Кого он съел?");
-    problems.last().setSolution("Свой хвост, очевидно.");
-
-    problems.append(Problem("Сопротивление бесполезно!"));
-    problems.last().setTask("На рисунке изображена схема. Найти ее.");
-    problems.last().setSolution("404: схема не найдена. (Это шутка, а не реальный ответ БД)");
-
-    problems.append(Problem("Вечный вопрос"));
-    problems.last().setTask("Любит ли меня жена?");
-    problems.last().setSolution("Чья?");
-}
-*/
