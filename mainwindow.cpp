ba1f2011ac50faa7e5fc224a31c9dacb6861bf16
Alexander Dergachev <dergachev88@gmail.com>
2020-07-19 11:16:49 +0300

    db saved to file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -52,49 +52,12 @@ MainWindow::MainWindow(QWidget *parent)
                     "This demo needs the SQLITE driver"
                     );
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(":memory:");
+    db.setDatabaseName("c:\\users\\lidiy\\Composer\\tasks.db");
 
     if (!db.open())
         processError(db.lastError());
 
     updateTables();
-/*
-    QSqlQuery q;
-    if (!q.exec(QLatin1String("create table books(id integer primary key, title varchar, author integer, genre integer, year integer, rating integer)")))
-        return q.lastError();
-    if (!q.exec(QLatin1String("create table authors(id integer primary key, name varchar, birthdate date)")))
-        return q.lastError();
-    if (!q.exec(QLatin1String("create table genres(id integer primary key, name varchar)")))
-        return q.lastError();
-*/
-/*    if (!q.prepare(QLatin1String("insert into authors(name, birthdate) values(?, ?)")))
-        return q.lastError();
-    QVariant asimovId = addAuthor(q, QLatin1String("Isaac Asimov"), QDate(1920, 2, 1));
-    QVariant greeneId = addAuthor(q, QLatin1String("Graham Greene"), QDate(1904, 10, 2));
-    QVariant pratchettId = addAuthor(q, QLatin1String("Terry Pratchett"), QDate(1948, 4, 28));
-
-    if (!q.prepare(QLatin1String("insert into genres(name) values(?)")))
-        return q.lastError();
-    QVariant sfiction = addGenre(q, QLatin1String("Science Fiction"));
-    QVariant fiction = addGenre(q, QLatin1String("Fiction"));
-    QVariant fantasy = addGenre(q, QLatin1String("Fantasy"));
-
-    if (!q.prepare(QLatin1String("insert into books(title, year, author, genre, rating) values(?, ?, ?, ?, ?)")))
-        return q.lastError();
-    addBook(q, QLatin1String("Foundation"), 1951, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation and Empire"), 1952, asimovId, sfiction, 4);
-    addBook(q, QLatin1String("Second Foundation"), 1953, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation's Edge"), 1982, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Foundation and Earth"), 1986, asimovId, sfiction, 4);
-    addBook(q, QLatin1String("Prelude to Foundation"), 1988, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("Forward the Foundation"), 1993, asimovId, sfiction, 3);
-    addBook(q, QLatin1String("The Power and the Glory"), 1940, greeneId, fiction, 4);
-    addBook(q, QLatin1String("The Third Man"), 1950, greeneId, fiction, 5);
-    addBook(q, QLatin1String("Our Man in Havana"), 1958, greeneId, fiction, 4);
-    addBook(q, QLatin1String("Guards! Guards!"), 1989, pratchettId, fantasy, 3);
-    addBook(q, QLatin1String("Night Watch"), 2002, pratchettId, fantasy, 3);
-    addBook(q, QLatin1String("Going Postal"), 2004, pratchettId, fantasy, 3);
-*/
 }
 
 MainWindow::~MainWindow()
@@ -118,7 +81,7 @@ void MainWindow::request()
         for (int idx = 0; idx < record.count(); ++idx)
         {
             str.append(record.value(idx).toString());
-            str.append(idx == record.count() ? '\n' : ' ');
+            str.append(idx == record.count() ? "\n" : " || ");
         }
         textEditResponse->append(str);
     }
